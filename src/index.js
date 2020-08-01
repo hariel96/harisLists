df927debb7e928bd7e07bb16c9e76b5bbd14a713
hariel <hariel96@gmail.com>
2020-08-01 20:31:30 -0300

    Atualizando o BD

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/home';
-import CadastroVideo from './pages/cadastro/Video'
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
 const Pagina404 = () => (
   <div>
-    <h1>Error 404 - Página não encontrada. </h1><br/>
+    <h1>Error 404 - Página não encontrada. </h1>
+    <br />
     <h4>A url especificada não foi encontrada, tente algo como:</h4>
-    <p>https://haris-lists.vercel.app/home</p>
-
+    <p>https://harislist.vercel.app/home</p>
   </div>
-  );
+);
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/home" component={Home}/>
-      <Route path="/cadastro/video" component={CadastroVideo}/>
-      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
-      <Route component={Pagina404}/>
-    
+      <Route path="/home" component={Home} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
